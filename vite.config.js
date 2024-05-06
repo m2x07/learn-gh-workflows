82eeab90ba9b9d1e2995442ffbda6680d66e8a60
m2x07 <meet2x07@gmail.com>
2024-05-06 14:30:10 +0530

    test

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/learn-gh-workflows/",
-  plugins: [react()],
+    plugins: [react()],
 });
